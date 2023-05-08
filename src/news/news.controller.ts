fb1f0b31582b044712402f419c810f63c7e48a01
Nikita Gurskiy <supergurskiy@gmail.com>
2023-05-08 22:50:11 +0200

    nest js go one

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param, Post, Body } from '@nestjs/common';
+/* eslint-disable prettier/prettier */
+import { Controller, Get, Param, Post, Body, Delete, } from '@nestjs/common';
 import { News, NewsService } from './news.service';
 
 @Controller('news')
@@ -10,8 +11,24 @@ export class NewsController {
     const idInt = parseInt(id);
     return this.newsService.find(idInt);
   }
+
+  @Get()
+  findAll(): string {
+    return this.newsService.findAll();
+  }
+  
   @Post()
-  create(@Body() news: News): News {
-    return this.newsService.create(news);
+  create(@Body() news: News): string {
+    const isCreated = this.newsService.create(news);
+    return isCreated ? 'Vse dobavleno STATUS CODE: 200' : 'Proizoshla oshibka STATUS CODE: 500';
+  }
+  @Delete('/:id')
+  remove(@Param('id') id: string): string {
+    const idInt = parseInt(id);
+    const isRemoved = this.newsService.remove(idInt);
+    return isRemoved ? 'Novost udalena' : 'Peredan neverniy ID';
   }
 }
+// const error = new Error("message")
+// error.code = "YOUR_STATUS_CODE"
+// throw error;
--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,11 +1,28 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
 
 export interface News {
   id: number;
-  title: string;
-  description: string;
-  author: string;
+  title?: string;
+  description?: string;
+  author?: string;
   countView?: number;
+}
+function getRandomInt(min: number, max: number): number {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+function getRandomString(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    return result;
 }
 @Injectable()
 export class NewsService {
@@ -20,10 +37,29 @@ export class NewsService {
   ];
 
   create(news: News): News {
-    this.news.push(news);
-    return news;
+    const countView = getRandomInt(0, 99999);
+    const title = getRandomString(6)
+    const description = getRandomString(25)
+    const author = getRandomString(6)
+    const finalNews = {
+        ...news, 
+        title: title,
+        description: description,
+        author: author,
+        countView: countView,
+    }
+    this.news.push(finalNews);
+    return finalNews;
   }
   find(id: News['id']): News | undefined {
     return this.news.find((news: News) => news.id === id);
+  }
+  remove(id: News['id']): boolean {
+    const indexRemoveNews = this.news.findIndex((news: News) => news.id === id);
+    if (indexRemoveNews !== -1) {
+      this.news.splice(indexRemoveNews, 1);
+      return true;
+    }
+    return false;
   }
 }
