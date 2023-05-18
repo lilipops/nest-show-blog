afa4e27e8f6273a638ce7512b549e95a89d7a442
Nikita Gurskiy <supergurskiy@gmail.com>
2023-05-18 23:42:42 +0200

    triying change comment

--- src/news/comments/comments.controller.ts
+++ src/news/comments/comments.controller.ts
@@ -1,20 +1,24 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Post, Param, Body, Get, Delete } from '@nestjs/common';
+import { Controller, Post, Param, Body, Get, Delete, Put } from '@nestjs/common';
 import { CommentsService } from './comments.service'
 import { Comment } from './comments.service'
+
+
 @Controller('comments')
 export class CommentsController {
     constructor(private readonly commentsService: CommentsService) {}
     @Post('/api/:idNews') 
     create(@Param('idNews') idNews: string, @Body() comment: Comment ) {
         const idNewsInt = parseInt(idNews)
+        console.log(idNewsInt)
+        console.log(comment)
         return this.commentsService.create(idNewsInt, comment)
     }
     @Get('/api/details/:idNews') 
     get(@Param('idNews') idNews: string) {
         const idNewsInt = parseInt(idNews)
-        return this.commentsService.find(idNewsInt)
-      
+        const comments =  this.commentsService.find(idNewsInt)
+        console.log(comments)
     }
     @Delete('/api/details/:idNews/:idComment') 
     remove(
@@ -25,5 +29,15 @@ export class CommentsController {
         const idCommentsInt = parseInt(idComment)
         return this.commentsService.remove(idNewsInt, idCommentsInt) 
       
+    }
+    @Put('/api/details/:idNews/:idComment') 
+    edit(
+        @Param('idNews') idNews: string, 
+        @Param('idComment') idComment: string,
+        @Body() comment: Comment,
+    ) {
+        const idInt = parseInt(idNews)
+        const idCommentsInt = parseInt(idComment)
+        return this.commentsService.edit(idInt, idCommentsInt, comment)
     }
 }
--- src/news/comments/comments.service.ts
+++ src/news/comments/comments.service.ts
@@ -3,10 +3,9 @@
 import { Injectable } from '@nestjs/common';
 import { getRandomInt } from '../news.service';
 export type Comment = {
-    id?: number;
+    id: number;
     message: string;
     author: string;
-
 }
 @Injectable()
 export class CommentsService {
@@ -17,9 +16,14 @@ export class CommentsService {
         if (!this.comments[idNews]) {
             this.comments[idNews] = [];
         }
+        const id = getRandomInt(1, 9999);
+        const finalComment ={
+            ...comment,
+            id: id,
+        }
 
-        this.comments[idNews].push({...comment, id: getRandomInt() });
-        return 'The comment was created'
+        this.comments[idNews].push(finalComment);
+        return `the comment was created and has ID ${id}`
     }
     find(idNews: number): Comment[] | undefined {
         return this.comments[idNews] || undefined;
@@ -29,9 +33,27 @@ export class CommentsService {
             return null
         }
         const indexComment = this.comments[idNews].findIndex((c) => c.id === idComment)
+        console.log(indexComment + ' index comment ')
         if (indexComment === -1) {
             return null;
         }
         return this.comments[idNews].splice(indexComment, 1);
+    }
+    edit(idNews: number, idComment: number, comment: Comment): Comment[] | null {
+        const indexEditableNews = this.comments[idNews].findIndex((c) => c.id === idComment)
+        console.log(indexEditableNews)
+        if (indexEditableNews !== -1) {
+            console.log(this.comments[idNews] + ' //11')
+            const updatedComment = this.comments[indexEditableNews] = {
+                ...this.comments[indexEditableNews],
+                ...comment,
+               id: idComment,
+            }
+            console.log('hola' + idComment)
+            console.log(comment)
+            console.log(updatedComment)
+            return updatedComment
+        }
+        return null
     }
 }
